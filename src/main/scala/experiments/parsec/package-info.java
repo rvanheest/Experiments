b5458bcdf8b0f4892e28e52c3e77f101ac0f36fa
Richard van Heest <A.w.m.vanHeest@student.tudelft.nl>
2016-02-20 21:31:48 +0100

    package-info for Parsec

---
+++ src/main/scala/experiments/parsec/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Inspired by
+ *  * http://research.microsoft.com/pubs/65201/parsec-paper-letter.pdf
+ *  * https://channel9.msdn.com/Series/C9-Lectures-Erik-Meijer-Functional-Programming-Fundamentals/C9-Lectures-Dr-Erik-Meijer-Functional-Programming-Fundamentals-Chapter-8-of-13
+ *  * http://charlieharvey.org.uk/page/naive_xml_parser_with_haskell_parsec_and_perl_regexen_part_one_haskell
+ */
+package experiments.parsec;
