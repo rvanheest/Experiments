e6abcaab3d7fe9fe16972e1ad8ed4752753e3572
Richard van Heest <richard.van.heest@dans.knaw.nl>
2016-05-09 08:37:14 +0200

    extending the State class with a companion object

--- src/main/scala/experiments/monadics/instances/State.scala
+++ src/main/scala/experiments/monadics/instances/State.scala
@@ -2,6 +2,14 @@ package experiments.monadics.instances
 
 class State[S, A](val state: S => (A, S)) {
   def run(s: S): (A, S) = state(s)
+
   def evaluate(s: S): A = state(s)._1
+
   def execute(s: S): S = state(s)._2
+}
+
+object State {
+  def get[S]: State[S, S] = new State(s => (s, s))
+
+  def put[S](newState: S): State[S, Unit] = new State(_ => ((), newState))
 }
