71bcbcaf268911602b1a4d72ef736af938d80ef1
Richard van Heest <richard.v.heest@gmail.com>
2016-05-30 22:42:53 +0200

    Some rewrites on StateT

--- src/main/scala/experiments/transformers/StateT.scala
+++ src/main/scala/experiments/transformers/StateT.scala
@@ -14,8 +14,17 @@ class StateT[S, A, M[_]](val state: S => M[(A, S)])(implicit m: MonadPlus[M]) {
 
   def execute(s: S): M[S] = m.map(state(s))(_._2)
 
+  def orElse[B >: A](other: => StateT[S, B, M]): StateT[S, B, M] = this <|> other
+  def <|>[B >: A](other: => StateT[S, B, M]): StateT[S, B, M] = {
+    lift(st => m.orElse(run(st), other.run(st)))
+  }
+
   def map[B](f: A => B): StateT[S, B, M] = {
     lift(st => m.map(state(st)) { case (a, s) => (f(a), s) })
+  }
+
+  def doOnNext[Ignore](f: A => Ignore): StateT[S, A, M] = {
+    map(a => { f(a); a })
   }
 
   def flatMap[B](f: A => StateT[S, B, M]): StateT[S, B, M] = this >>= f
@@ -24,25 +33,16 @@ class StateT[S, A, M[_]](val state: S => M[(A, S)])(implicit m: MonadPlus[M]) {
   }
 
   def >>[B](other: => StateT[S, B, M]): StateT[S, B, M] = {
-    >>= (_ => other)
+    this >>= (_ => other)
   }
 
   def <<[B](other: => StateT[S, B, M]): StateT[S, A, M] = {
-    >>= (x => other >>= (_ => from(x)))
-  }
-
-  def orElse[B >: A](other: => StateT[S, B, M]): StateT[S, B, M] = this <|> other
-  def <|>[B >: A](other: => StateT[S, B, M]): StateT[S, B, M] = {
-    lift(st => m.orElse(run(st), other.run(st)))
+    this >>= (x => other >> from(x))
   }
 
   def filter(predicate: A => Boolean): StateT[S, A, M] = satisfy(predicate)
   def satisfy(predicate: A => Boolean): StateT[S, A, M] = {
-    >>= (x => if (predicate(x)) from(x) else failure)
-  }
-
-  def noneOf(as: List[A]): StateT[S, A, M] = {
-    satisfy (!as.contains(_))
+    this >>= (x => if (predicate(x)) from(x) else failure)
   }
 
   def maybe: StateT[S, Option[A], M] = map(Option(_)) <|> from(Option.empty)
@@ -50,7 +50,10 @@ class StateT[S, A, M[_]](val state: S => M[(A, S)])(implicit m: MonadPlus[M]) {
   def many: StateT[S, List[A], M] = atLeastOnce <|> from(Nil)
 
   def atLeastOnce: StateT[S, List[A], M] = {
-    >>= (v => many map (v :: _))
+    for {
+      x <- this
+      xs <- many
+    } yield x :: xs
   }
 
   def takeUntil(predicate: A => Boolean): StateT[S, List[A], M] = {
@@ -66,21 +69,28 @@ class StateT[S, A, M[_]](val state: S => M[(A, S)])(implicit m: MonadPlus[M]) {
   }
 
   def separatedBy1[Sep](sep: StateT[S, Sep, M]): StateT[S, List[A], M] = {
-    >>= (x => (sep >> this).many.map(x :: _))
+    for {
+      x <- this
+      xs <- (sep >> this).many
+    } yield x :: xs
   }
 
   def skipMany: StateT[S, Unit, M] = this >> skipMany <|> from(())
 }
 object StateT {
   def lift[S, A, M[_]](x: S => M[(A, S)])(implicit m: MonadPlus[M]): StateT[S, A, M] = {
-    new StateT[S, A, M](x)
+    new StateT(x)
+  }
+
+  def apply[S, A, M[_]](x: S => M[(A, S)])(implicit m: MonadPlus[M]): StateT[S, A, M] = {
+    new StateT(x)
   }
 
   def from[S, A, M[_]](a: A)(implicit m: MonadPlus[M]): StateT[S, A, M] = {
-    new StateT[S, A, M](s => m.create((a, s)))
+    StateT(s => m.create((a, s)))
   }
 
   def failure[S, A, M[_]](implicit m: MonadPlus[M]): StateT[S, A, M] = {
-    new StateT(_ => m.empty)
+    StateT(_ => m.empty)
   }
 }
