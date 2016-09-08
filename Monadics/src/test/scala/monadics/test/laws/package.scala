2f7700da35be949b03e9493367ddc08ee505736c
Richard van Heest <richard.v.heest@gmail.com>
2016-09-08 21:43:25 +0200

    better arbitrary tree generation
    the former implementation caused a StackOverflowError on the monadic
    associativity law

--- Monadics/src/test/scala/monadics/test/laws/package.scala
+++ Monadics/src/test/scala/monadics/test/laws/package.scala
@@ -5,6 +5,7 @@ import monadics.instances.StateT.StateTMonadPlus
 import monadics.instances._
 import org.scalacheck.{Arbitrary, Gen}
 import org.scalacheck.Arbitrary._
+import org.scalacheck.Gen._
 
 package object laws {
 
@@ -25,6 +26,22 @@ package object laws {
 	}
 
 	implicit def arbTree[T](implicit a: Arbitrary[T]): Arbitrary[Tree[T]] = {
-		Arbitrary(Gen.lzy(Gen.oneOf(arbitrary[T].map(Leaf(_)), arbitrary[(Tree[T], Tree[T])].map((Branch[T] _).tupled))))
+		val ts = arbitrary[T]
+
+		def leafs: Gen[Leaf[T]] = ts.map(Leaf(_))
+
+		def genBranch(maxDepth: Int): Gen[Branch[T]] = for {
+			depthL <- Gen.choose(0, maxDepth - 1)
+			depthR <- Gen.choose(0, maxDepth - 1)
+			left <- genTree(depthL)
+			right <- genTree(depthR)
+		} yield Branch(left, right)
+
+		def genTree(maxDepth: Int): Gen[Tree[T]] = {
+			if (maxDepth == 0) leafs
+			else Gen.oneOf(leafs, genBranch(maxDepth))
+		}
+
+		Arbitrary(Gen.sized { size => genTree(size) })
 	}
 }
