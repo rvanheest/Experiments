a9643798b4c176451ad25537a2ac805ba644474d
Richard van Heest <richard.v.heest@gmail.com>
2016-11-24 23:36:24 +0100

    copy paste error

--- Monadics/src/main/scala/monadics/instances/monoids/All.scala
+++ Monadics/src/main/scala/monadics/instances/monoids/All.scala
@@ -2,7 +2,7 @@ package monadics.instances.monoids
 
 import monadics.structures.Monoid
 
-case class All(any: Boolean)(implicit monoid: Monoid[All]) {
+case class All(all: Boolean)(implicit monoid: Monoid[All]) {
 
   def combine(other: => All): All = {
     monoid.combine(this, other)
@@ -13,6 +13,6 @@ object All {
   def empty(implicit monoid: Monoid[All]): All = monoid.empty
 
   implicit def anyIsMonoid: Monoid[All] = {
-    Monoid.create(All(any = true))((x, y) => All(x.any && y.any))
+    Monoid.create(All(all = true))((x, y) => All(x.all && y.all))
   }
 }
