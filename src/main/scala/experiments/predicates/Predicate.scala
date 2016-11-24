8a9e92298e61757179b47c1c1053158b07fa6de3
Richard van Heest <richard.van.heest@dans.knaw.nl>
2016-11-24 06:39:43 +0100

    remove old code

--- src/main/scala/experiments/predicates/Predicate.scala
+++ src/main/scala/experiments/predicates/Predicate.scala
@@ -56,23 +56,3 @@ object Predicate {
     values.partition(predicate)
   }
 }
-
-object Demo extends App {
-
-  import Predicate._
-
-  val p1 = (i: Int) => i > 0
-  val p2 = (i: Int) => i % 2 == 0
-
-  val p3 = !p1
-  val p4 = p1 and p2
-  val p5 = p1 or p2
-  val p6 = p1 xor p2
-  val p7 = p1 nor p2
-  val p8 = p1 nand p2
-  val p9 = p1 xnor p2
-  val p10 = p1 -> p2
-  val p11 = p1 <-> p2
-
-
-}
