6a218b052485105815762b958f3ffa87150ac02c
Richard van Heest <richard.v.heest@gmail.com>
2016-12-11 17:09:10 +0100

    collect in MonadFilter

--- Monadics/src/main/scala/monadics/structures/MonadFilter.scala
+++ Monadics/src/main/scala/monadics/structures/MonadFilter.scala
@@ -10,7 +10,11 @@ trait MonadFilter[MF[_]] extends Monad[MF] {
     flatMap(mf)(a => if (predicate(a)) create(a) else empty[A])
   }
 
-  def filterNot[A](mp: MF[A])(predicate: A => Boolean): MF[A] = {
-    filter(mp)(!predicate(_))
+  def filterNot[A](mf: MF[A])(predicate: A => Boolean): MF[A] = {
+    filter(mf)(!predicate(_))
+  }
+
+  def collect[A, B](mf: MF[A])(pf: PartialFunction[A, B]): MF[B] = {
+    flatMap(mf)(a => if (pf.isDefinedAt(a)) create(pf(a)) else empty[B])
   }
 }
