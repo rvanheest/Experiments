dd8f7027aa090cbcc4872176685e602119ca3a74
Richard van Heest <richard.van.heest@dans.knaw.nl>
2017-03-14 09:21:44 +0100

    type renaming

--- src/main/scala/experiments/pickling/xml/Pickle.scala
+++ src/main/scala/experiments/pickling/xml/Pickle.scala
@@ -1,51 +1,50 @@
 package experiments.pickling.xml
 
-trait Pickle[T, State] { self =>
-	def pickle(t: T, state: State): State
+trait Pickle[A, State] { self =>
+	def pickle(a: A, state: State): State
 
-	def maybe[S](implicit ev: S =:= T): Pickle[Option[S], State] = new Pickle[Option[S], State] {
-	// TODO can we get rid of this evidence?
-		override def pickle(optS: Option[S], state: State): State = {
+	def maybe[B](implicit ev: B =:= A): Pickle[Option[B], State] = new Pickle[Option[B], State] {
+		// TODO can we get rid of this evidence?
+		override def pickle(optS: Option[B], state: State): State = {
 			optS.map(x => self.pickle(ev(x), state)).getOrElse(state)
 		}
 	}
 
-	def seq[R](f: R => T)(g: T => Pickle[R, State]): Pickle[R, State] = new Pickle[R, State] {
-		override def pickle(r: R, s: State): State = {
-			val t = f(r)
-			self.pickle(t, g(t).pickle(r, s))
+	def seq[B](f: B => A)(g: A => Pickle[B, State]): Pickle[B, State] = new Pickle[B, State] {
+		override def pickle(b: B, s: State): State = {
+			val t = f(b)
+			self.pickle(t, g(t).pickle(b, s))
 		}
 	}
 
-	def pair[B](pb: Pickle[B, State]): Pickle[(T, B), State] = {
-		this.seq[(T, B)]({ case (t, _) => t })(t =>
-			pb.seq[(T, B)]({ case (_, b) => b })(b =>
-				Pickle.lift(t, b)))
+	def pair[B](pb: Pickle[B, State]): Pickle[(A, B), State] = {
+		this.seq[(A, B)]({ case (a, _) => a })(a =>
+			pb.seq[(A, B)]({ case (_, b) => b })(b =>
+				Pickle.lift(a, b)))
 	}
 
-	def triple[B, C](pb: Pickle[B, State], pc: Pickle[C, State]): Pickle[(T, B, C), State] = {
-		this.seq[(T, B, C)]({ case (t, _, _) => t })(t =>
-			pb.seq[(T, B, C)]({ case (_, b, _) => b })(b =>
-				pc.seq[(T, B, C)]({ case (_, _, c) => c })(c =>
-					Pickle.lift(t, b, c))))
+	def triple[B, C](pb: Pickle[B, State], pc: Pickle[C, State]): Pickle[(A, B, C), State] = {
+		this.seq[(A, B, C)]({ case (a, _, _) => a })(a =>
+			pb.seq[(A, B, C)]({ case (_, b, _) => b })(b =>
+				pc.seq[(A, B, C)]({ case (_, _, c) => c })(c =>
+					Pickle.lift(a, b, c))))
 	}
 
-	def quad[B, C, D](pb: Pickle[B, State], pc: Pickle[C, State], pd: Pickle[D, State]): Pickle[(T, B, C, D), State] = {
-		this.seq[(T, B, C, D)]({ case (t, _, _, _) => t })(t =>
-			pb.seq[(T, B, C, D)]({ case (_, b, _, _) => b })(b =>
-				pc.seq[(T, B, C, D)]({ case (_, _, c, _) => c })(c =>
-					pd.seq[(T, B, C, D)]({ case (_, _, _, d) => d })(d =>
-						Pickle.lift(t, b, c, d)))))
+	def quad[B, C, D](pb: Pickle[B, State], pc: Pickle[C, State], pd: Pickle[D, State]): Pickle[(A, B, C, D), State] = {
+		this.seq[(A, B, C, D)]({ case (a, _, _, _) => a })(a =>
+			pb.seq[(A, B, C, D)]({ case (_, b, _, _) => b })(b =>
+				pc.seq[(A, B, C, D)]({ case (_, _, c, _) => c })(c =>
+					pd.seq[(A, B, C, D)]({ case (_, _, _, d) => d })(d =>
+						Pickle.lift(a, b, c, d)))))
 	}
 
-	def wrap[B](f: T => B): WrapBuilder[T, B, State] = {
-		new WrapBuilder(this, f)
-//		this.seq[B](b => if (g isDefinedAt b) g(b) else sys.error("undefined"))(t => Pickle.lift(f(t)))
+	def wrap[B](f: A => B): WrapBuilder[B] = {
+		new WrapBuilder(self, f)
 	}
 
-	class WrapBuilder[A, B, St](pickle: Pickle[A, St], f: A => B) {
-		def unwrap(g: PartialFunction[B, A]): Pickle[B, St] = {
-			pickle.seq[B](b => if (g isDefinedAt b) g(b) else sys.error("undefined"))(t => Pickle.lift(f(t)))
+	class WrapBuilder[B](pickle: Pickle[A, State], f: A => B) {
+		def unwrap(g: PartialFunction[B, A]): Pickle[B, State] = {
+			pickle.seq[B](b => if (g isDefinedAt b) g(b) else sys.error("undefined"))(t => Pickle.lift[B, State](f(t)))
 		}
 	}
 }
--- src/main/scala/experiments/pickling/xml/XmlPickle.scala
+++ src/main/scala/experiments/pickling/xml/XmlPickle.scala
@@ -4,15 +4,15 @@ import scala.xml._
 
 object XmlPickle {
 
-	type XmlPickle[T] = Pickle[T, Seq[Node]]
+	type XmlPickle[A] = Pickle[A, Seq[Node]]
 
-	def string(name: String): XmlPickle[String] = new Pickle[String, Seq[Node]] {
+	def string(name: String): XmlPickle[String] = new XmlPickle[String] {
 		override def pickle(s: String, xml: Seq[Node]): Seq[Node] = {
 			<xml>{s}</xml>.copy(label = name) ++ xml
 		}
 	}
 
-	def attribute(name: String): XmlPickle[String] = new Pickle[String, Seq[Node]] {
+	def attribute(name: String): XmlPickle[String] = new XmlPickle[String] {
 		override def pickle(s: String, xml: Seq[Node]): Seq[Node] = {
 			xml.headOption map {
 				case elem: Elem => elem % new UnprefixedAttribute(name, s, Null) ++ xml.tail
@@ -21,7 +21,7 @@ object XmlPickle {
 		}
 	}
 
-	def attribute(prefix: String, name: String): XmlPickle[String] = new Pickle[String, Seq[Node]] {
+	def attribute(prefix: String, name: String): XmlPickle[String] = new XmlPickle[String] {
 		override def pickle(s: String, xml: Seq[Node]): Seq[Node] = {
 			xml.headOption map {
 				case elem: Elem => elem % new PrefixedAttribute(prefix, name, s, Null) ++ xml.tail
@@ -30,7 +30,7 @@ object XmlPickle {
 		}
 	}
 
-	def inside[A](name: String)(pickleA: XmlPickle[A]): XmlPickle[A] = new Pickle[A, Seq[Node]] {
+	def inside[A](name: String)(pickleA: XmlPickle[A]): XmlPickle[A] = new XmlPickle[A] {
 		override def pickle(a: A, xml: Seq[Node]): Seq[Node] = {
 			<xml>{pickleA.pickle(a, Nil)}</xml>.copy(label = name) ++ xml
 		}
