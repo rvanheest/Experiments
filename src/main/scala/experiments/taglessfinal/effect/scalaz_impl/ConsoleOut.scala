1c1e36c69b8c450ef531f00ebc6db2aafc11c196
Richard van Heest <richard.v.heest@gmail.com>
2018-11-03 20:12:24 +0100

    Scalaz implementation of tagless final example

---
+++ src/main/scala/experiments/taglessfinal/effect/scalaz_impl/ConsoleOut.scala
@@ -0,0 +1,29 @@
+package experiments.taglessfinal.effect.scalaz_impl
+
+sealed trait ConsoleOut {
+  def en: String
+}
+
+object ConsoleOut {
+  case object WhatIsYourName extends ConsoleOut {
+    def en = "What is your name?"
+  }
+  case class WelcomeToGame(name: String) extends ConsoleOut {
+    def en: String = s"Hello, $name, welcome to the game!"
+  }
+  case class PleaseGuess(name: String) extends ConsoleOut {
+    def en: String = s"Dear $name, please guess a number from 1 to 5:"
+  }
+  case class ThatIsNotValid(name: String) extends ConsoleOut {
+    def en: String = s"You did not enter a number, $name!"
+  }
+  case class YouGuessedRight(name: String) extends ConsoleOut {
+    def en: String = s"You guessed right, $name!"
+  }
+  case class YouGuessedWrong(name: String, num: Int) extends ConsoleOut {
+    def en: String = s"You guessed wrong, $name! The number was: $num."
+  }
+  case class DoYouWantToContinue(name: String) extends ConsoleOut {
+    def en: String = s"Do you want to continue, $name? [y/n]"
+  }
+}
