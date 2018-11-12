917d5a48e399b20544bdcf4a55a022fec61a5ad1
Richard van Heest <richard.van.heest@dans.knaw.nl>
2018-11-12 14:49:51 +0100

    remove old syntax

--- src/main/scala/experiments/typeclasses/expressionProblem/base/Expr.scala
+++ src/main/scala/experiments/typeclasses/expressionProblem/base/Expr.scala
@@ -9,8 +9,4 @@ object Expr {
   def apply[E](f: E => Int): Expr[E] = new Expr[E] {
     def eval(e: E): Int = f(e)
   }
-
-  implicit class ExprSyntax[E: Expr](val expr: E) extends AnyVal {
-    def evaluate: Int = Expr[E].eval(expr)
-  }
 }
