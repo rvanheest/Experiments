3f33188669c12218786d081cf041c495b81c186a
Richard van Heest <richard.van.heest@dans.knaw.nl>
2019-04-06 13:05:12 +0200

    performance measurements

--- src/main/scala/experiments/performance/AppendPerformance.scala
+++ src/main/scala/experiments/performance/AppendPerformance.scala
@@ -2,7 +2,7 @@ package experiments.performance
 
 import scala.collection.mutable.ListBuffer
 
-object AppendPerformance extends App {
+object AppendPerformance extends App with Measurement {
   
   val list1 = (1 to 2000000).to[ListBuffer]
   val list2 = (2000001 to 4000000).to[ListBuffer]
@@ -18,13 +18,5 @@ object AppendPerformance extends App {
 
   timed("task 3") {
     list1 ++ list2 ++ list3
-  }
-  
-  def timed[A](name: String)(task: => A): Unit = {
-    println(s"start $name")
-    val start = System.nanoTime()
-    task
-    val end = System.nanoTime()
-    println(s"$name took ${ end - start } nanoseconds (${ (end - start) / 1E9 } seconds)")
   }
 }
