884f47689411c410196da25b0dbd7836b0149e52
Richard van Heest <rvanheest@users.noreply.github.com>
2022-09-15 08:30:20 +0200

    upgrade Mozilla User-Agent

--- src/main/scala/experiments/podcast/auto/package.scala
+++ src/main/scala/experiments/podcast/auto/package.scala
@@ -8,7 +8,7 @@ package object auto {
   implicit class URLExtension(val url: URL) {
     def toInputStream: InputStream = {
       val uc = url.openConnection()
-      uc.addRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)")
+      uc.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.3; Win64; x64)")
       uc.getInputStream
     }
   }
